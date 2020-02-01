2f3cb33710cbc66713d62b83ed9ad271fc2ce4ad
venkateshsoal <59640240+venkateshsoal@users.noreply.github.com>
2020-02-02 00:27:13 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
     createDeck();
     //Delear();
     //Player();
-    Start();
+    //Start();
     var game_p_count;
     //Player();
     //Hit();
